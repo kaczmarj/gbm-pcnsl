53e850e20d567d8755113f7aee77345830b0a961
Jakub Kaczmarzyk <jakubk@mit.edu>
2020-06-26 19:31:41 -0400

    ADD: example of predictions (#1)
    * rename file
    
    * add sample images and prediction output
    
    * add cli script to predict on images
    
    * move attribution to table with images
    
    * increase whitespace in prediction output
    
    * add warning that this project is only meant for research

--- README.md
+++ README.md
@@ -2,10 +2,29 @@
 
 Classify between glioblastoma (GBM) and primary central nervous system lymphoma (PCNSL) in contrast-enhanced T1-weighted MRI.
 
+```diff
+! CAUTION: this tool is not a medical product and is only intended for research purposes.
+```
+
+Glioblastoma       |  Primary central nervous system lymphoma
+:-----------------:|:----------------------------------------:
+![Glioblastoma](images/sample-gbm.jpeg)  |  ![Primary central nervous system lymphoma](images/sample-pcnsl.jpeg)
+Case courtesy of Assoc Prof Frank Gaillard, <a href="https://radiopaedia.org/">Radiopaedia.org</a>. From the case <a href="https://radiopaedia.org/cases/5565">rID: 5565</a>. | Case courtesy of Dr Bruno Di Muzio, <a href="https://radiopaedia.org/">Radiopaedia.org</a>. From the case <a href="https://radiopaedia.org/cases/64657">rID: 64657</a>.
+
+
+```
+$ python predict.py savedmodels/augment images/sample-*.jpeg
+images/sample-gbm.jpeg                   100.00 % GBM
+images/sample-pcnsl.jpeg                 100.00 % PCNSL
+```
+
+_Note: the saved models are not part of this repository. Please contact the authors for access to the models._
+
+
 # Method
 
 1. Data were originally in PNG format.
-2. PNGs were processed and saved to HDF5 using the Jupyter Noteook `convert_to_hdf5.ipynb`.
+2. PNGs were processed and saved to HDF5 using the Jupyter Noteook `convert_images_to_hdf5.py`.
 3. [EfficientNetB4](https://arxiv.org/abs/1905.11946) (published in ICML 2019) architecture was used to classify between GBM and PCNSL.
     - Model implemented in `keras_applications` and accessed with tensorflow.
     - Model was trained using the script `train_efficientnetb4.py`.
---
+++ predict.py
@@ -0,0 +1,71 @@
+"""
+Give the prediction probability that an MR image contains glioblastoma (GBM)
+or primary central nervous system lymphoma (PCNSL).
+
+Images must be axial, T1-weighted, contrast-enhanced MR images.
+
+Usage: python predict.py MODELPATH IMAGEPATH...
+"""
+
+import os
+from pathlib import Path
+import sys
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import tensorflow as tf
+
+REQUIRED_IMAGE_SIZE = (380, 380)
+
+
+def get_image(file_path):
+    img = tf.io.read_file(file_path)
+    img = tf.image.decode_image(img, channels=3, dtype=tf.float32)
+    # Transform to [0, 1].
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    img = tf.image.resize(img, REQUIRED_IMAGE_SIZE)
+    # Transform to [-1, 1].
+    img *= 2
+    img -= 1
+    return img
+
+
+def predict_on_images(file_paths):
+    model = tf.keras.models.load_model(model_path, compile=False)
+    s = "{: <40} {:.02f} % {}"
+    for file_path in file_paths:
+        img = get_image(file_path)
+        prob_pcnsl = model.predict(img[None])
+        prob_pcnsl = float(prob_pcnsl[0])
+        prob_gbm = 1 - prob_pcnsl
+        if prob_pcnsl > prob_gbm:
+            print(s.format(file_path, prob_pcnsl * 100, "PCNSL"))
+        else:
+            print(s.format(file_path, prob_gbm * 100, "GBM"))
+
+
+def main(model_path, file_paths):
+
+    if not Path(model_path).exists():
+        raise FileNotFoundError("model path not found: {}".format(model_path))
+    for file_path in file_paths:
+        if not Path(model_path).exists():
+            raise FileNotFoundError(
+                "image file not found: {}".format(file_path)
+            )
+
+    predict_on_images(file_paths)
+
+
+if __name__ == "__main__":
+
+    if "-h" in sys.argv or "--help" in sys.argv:
+        print(__docstring__)
+        sys.exit(0)
+    elif len(sys.argv) < 3:
+        print("usage: python {} MODELPATH IMAGEPATH...".format(sys.argv[0]))
+        sys.exit(1)
+    model_path = sys.argv[1]
+    file_paths = sys.argv[2:]
+
+    main(model_path, file_paths)
